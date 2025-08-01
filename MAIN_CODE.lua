6ac81e44bebd424bfa8d7d9e1e42cae1a5c9bd9d
Eloi Stree <eloistree@gmail.com>
2025-08-01 03:40:54 +0200

    Save first working prototype

--- MAIN_CODE.lua
+++ MAIN_CODE.lua
@@ -38,12 +38,15 @@ end)
 
 
 --------------------------------------------------------------------------
-local fontPath = "Interface\\AddOns\\ColorTelemetry\\Fonts\\VerticalB58.ttf"
+--local fontPath = "Interface\\AddOns\\ColorTelemetry\\Fonts\\VerticalB58.ttf"
+local fontPath = "Interface\\AddOns\\ColorTelemetry\\Fonts\\Vertical4BitsSquare.ttf"
+
+--local fontPath = "Interface\\AddOns\\ColorTelemetry\\Fonts\\free3of9.ttf"
 -- Create the main frame to take all the space
 local frameCode = CreateFrame("Frame", "BarcodeFrame", UIParent, "BackdropTemplate")
 frameCode:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 0, 0)
 frameCode:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", 0, 0)
-frameCode:SetHeight(UIParent:GetHeight() * 0.2)
+frameCode:SetHeight(UIParent:GetHeight() * 0.1)
 frameCode:Show()
 
 -- Remove resizing and moving
@@ -51,10 +54,51 @@ frameCode:SetMovable(false)
 frameCode:EnableMouse(false)
 frameCode:SetResizable(false)
 
+
+-- Make frameCode movable by dragging with mouse
+frameCode:SetMovable(true)
+frameCode:EnableMouse(true)
+frameCode:RegisterForDrag("LeftButton")
+frameCode:SetScript("OnDragStart", function(self)
+    self:StartMoving()
+end)
+frameCode:SetScript("OnDragStop", function(self)
+    self:StopMovingOrSizing()
+end)
+
+
+-- Create a frame for the text below frameCode
+local textFrame = CreateFrame("Frame", "NormalTextFrame", frameCode, "BackdropTemplate")
+textFrame:SetPoint("TOPLEFT", frameCode, "BOTTOMLEFT", 0, 0)
+textFrame:SetPoint("TOPRIGHT", frameCode, "BOTTOMRIGHT", 0, 0)
+textFrame:SetHeight(30)
+textFrame:Show()
+
+-- Add a font string to display normal text
+textFrame.text = textFrame:CreateFontString(nil, "OVERLAY")
+textFrame.text:SetFont("Fonts\\FRIZQT__.TTF", 18, "OUTLINE")
+textFrame.text:SetPoint("LEFT", textFrame, "LEFT", 10, 0)
+textFrame.text:SetPoint("RIGHT", textFrame, "RIGHT", -10, 0)
+textFrame.text:SetJustifyH("LEFT")
+textFrame.text:SetText("Normal text goes here")
+
+function set_normal_text(text)
+    textFrame.text:SetText(text)
+end
+
+
+
+
+backgroundColor = {1 , 1 , 1 , 1}
+backgroundColor = {1 , 0 , 1 , 1}
+backgroundColor = {0,0,0 , 1}
+backgroundColor = {0,0,0 , 0}
+ 
+
 -- Background (optional)
 frameCode.bg = frameCode:CreateTexture(nil, "BACKGROUND")
 frameCode.bg:SetAllPoints()
-frameCode.bg:SetColorTexture(0.1,0.1,0.1, 1)
+frameCode.bg:SetColorTexture(unpack(backgroundColor))
 
 -- Barcode font size and spacing
 local function getBarHeight()
@@ -64,7 +108,7 @@ end
 
 local function createBarcodeFontString(parent, color)
     local fs = parent:CreateFontString(nil, "OVERLAY")
-    fs:SetFont(fontPath, getBarHeight(), "")
+    fs:SetFont(fontPath, getBarHeight(),  "OUTLINE, THICKOUTLINE")
     fs:SetText("")
     fs:SetTextColor(unpack(color))
     fs:SetSpacing(0)
@@ -75,141 +119,224 @@ local function createBarcodeFontString(parent, color)
     return fs
 end
 
-local brightness = 0.5
+local brightness = 0.65
 -- Create barcode font strings stacked vertically
 frameCode.text_black   = createBarcodeFontString(frameCode, {0, 0, 0, 1})
+frameCode.text_hide_zero    = createBarcodeFontString(frameCode, {0, 0, 0, 1})
 frameCode.text_red     = createBarcodeFontString(frameCode, {brightness, 0, 0, 1})
 frameCode.text_green   = createBarcodeFontString(frameCode, {0, brightness, 0, 1})
 frameCode.text_blue    = createBarcodeFontString(frameCode, {0, 0, brightness, 1})
+frameCode.text_cyan    = createBarcodeFontString(frameCode, {0, brightness, brightness, 1})
+frameCode.text_yellow  = createBarcodeFontString(frameCode, {brightness, brightness, 0, 1})
 frameCode.text_magenta = createBarcodeFontString(frameCode, {brightness, 0, brightness, 1})
+frameCode.text_white   = createBarcodeFontString(frameCode, {1, 1, 1, 1})
 
 
--- Set random 128-byte text for each color barcode
-local function randomText(len)
-    local chars = {}
-    for i = 1, len do
-        
-        chars[i] = string.char(math.random(1, 127))
-        if i == 1 or i == len then
-            chars[i] = string.char(127)
+
+
+local brgb_color ={
+    {0, 0, 0, 1}, -- black
+    {1, 0, 0, 1}, -- red
+    {0, 1, 0, 1}, -- green
+    {0, 0, 1, 1}, -- blue
+    -- IF YOU WANT MORE BITS 
+    -- {0, 1, 1, 1}, -- cyan
+    -- {1, 1, 0, 1}, -- yellow
+    -- {1, 0, 1, 1}, -- magenta
+    -- {1, 1, 1, 1}  -- white
+}
+
+-- create enum back  red green blue
+local black = 0
+local red = 1
+local green = 2
+local blue = 3
+local cyan = 4
+local yellow = 5
+local magenta = 6
+local white = 7
+    
+function get_brgb_from_index(hh)
+    local bit1Frequence = hh % #brgb_color
+    local bit2Frequence = math.floor(hh / #brgb_color) % #brgb_color
+    local bit3Frequence = (math.floor(hh / (#brgb_color * #brgb_color)) % #brgb_color)
+    local bit4Frequence = (math.floor(hh / (#brgb_color * #brgb_color * #brgb_color)) % #brgb_color)
+    return bit1Frequence, bit2Frequence, bit3Frequence, bit4Frequence
+end
+
+function get_utf8_from_brgb_index(gg, colorid)
+    local b1, b2, b3, b4 = get_brgb_from_index(gg)
+
+    
+    if b1 ~= colorid and b2 ~= colorid and b3 ~= colorid and b4 ~= colorid then
+        return "a"
+    elseif b1 == colorid and b2 ~= colorid and b3 ~= colorid and b4 ~= colorid then
+        return "b"
+    elseif b1 ~= colorid and b2 == colorid and b3 ~= colorid and b4 ~= colorid then
+        return "c"
+    elseif b1 == colorid and b2 == colorid and b3 ~= colorid and b4 ~= colorid then
+        return "d"
+    elseif b1 ~= colorid and b2 ~= colorid and b3 == colorid and b4 ~= colorid then
+        return "e"
+    elseif b1 == colorid and b2 ~= colorid and b3 == colorid and b4 ~= colorid then
+        return "f"
+    elseif b1 ~= colorid and b2 == colorid and b3 == colorid and b4 ~= colorid then
+        return "g"
+    elseif b1 == colorid and b2 == colorid and b3 == colorid and b4 ~= colorid then
+        return "h"
+    elseif b1 ~= colorid and b2 ~= colorid and b3 ~= colorid and b4 == colorid then
+        return "i"
+    elseif b1 == colorid and b2 ~= colorid and b3 ~= colorid and b4 == colorid then
+        return "j"
+    elseif b1 ~= colorid and b2 == colorid and b3 ~= colorid and b4 == colorid then
+        return "k"
+    elseif b1 == colorid and b2 == colorid and b3 ~= colorid and b4 == colorid then
+        return "l"
+    elseif b1 ~= colorid and b2 ~= colorid and b3 == colorid and b4 == colorid then
+        return "m"
+    elseif b1 == colorid and b2 ~= colorid and b3 == colorid and b4 == colorid then
+        return "n"
+    elseif b1 ~= colorid and b2 == colorid and b3 == colorid and b4 == colorid then
+        return "o"
+    elseif b1 == colorid and b2 == colorid and b3 == colorid and b4 == colorid then
+        return "p"
+    end
+
+    return "a" -- Default case, should not happen
+end
+
+function set_text_to_display_brgb(text)
+    local black_string = "p"
+    local red_string = "p"
+    local green_string = "p"
+    local blue_string = "p"
+    local cyan_string = "p"
+    local yellow_string = "p"
+    local magenta_string = "p"
+    local white_string = "p"
+    local hide_zero = "p"
+    for i = 1, #text do
+        local char = text:sub(i, i)
+        local jj = string.byte(char) - 1 -- Convert to 0-based index
+        black_string   = black_string   .. get_utf8_from_brgb_index(jj, black)
+        red_string     = red_string     .. get_utf8_from_brgb_index(jj, red)
+        green_string   = green_string   .. get_utf8_from_brgb_index(jj, green)
+        blue_string    = blue_string    .. get_utf8_from_brgb_index(jj, blue)
+        cyan_string    = cyan_string    .. get_utf8_from_brgb_index(jj, cyan)
+        yellow_string  = yellow_string  .. get_utf8_from_brgb_index(jj, yellow)
+        magenta_string = magenta_string .. get_utf8_from_brgb_index(jj, magenta)
+        white_string   = white_string   .. get_utf8_from_brgb_index(jj, white)
+        hide_zero      = hide_zero      .. "0"
+    end
+    white_string = white_string .. "p"
+    frameCode.text_black:SetText(black_string)
+    frameCode.text_red:SetText(red_string)
+    frameCode.text_green:SetText(green_string)
+    frameCode.text_blue:SetText(blue_string)
+    frameCode.text_cyan:SetText(cyan_string)
+    frameCode.text_yellow:SetText(yellow_string)
+    frameCode.text_magenta:SetText(magenta_string)
+    frameCode.text_white:SetText(white_string)
+    frameCode.text_hide_zero:SetText(hide_zero)
+end
+
+local function set_text_to_display_all_index()
+    local black_string = ""
+    local red_string = ""
+    local green_string = ""
+    local blue_string = ""
+    local cyan_string = ""
+    local yellow_string = ""
+    local magenta_string = ""
+    local white_string = ""
+    local hide_zero     = ""
+    local ll = 0
+    for i = 1, 8096 do
+        if i == 2 then
+            ll = secondsCounter
+        else
+            ll = i - 1 -- Convert to 0-based index
         end
+        -- Get brgb character for each color
+        local char_black = get_utf8_from_brgb_index(ll, black)
+        local char_red = get_utf8_from_brgb_index(ll, red)
+        local char_green = get_utf8_from_brgb_index(ll, green)
+        local char_blue = get_utf8_from_brgb_index(ll, blue)
+        local char_cyan = get_utf8_from_brgb_index(ll, cyan)
+        local char_yellow = get_utf8_from_brgb_index(ll, yellow)
+        local char_magenta = get_utf8_from_brgb_index(ll, magenta)
+        local char_white = get_utf8_from_brgb_index(ll, white)
+        -- Always use '7' for black string
+        black_string = black_string .. char_black
+        red_string = red_string .. char_red
+        green_string = green_string .. char_green
+        blue_string = blue_string .. char_blue
+        cyan_string = cyan_string .. char_cyan
+        yellow_string = yellow_string .. char_yellow
+        magenta_string = magenta_string .. char_magenta
+        white_string = white_string .. char_white
+        hide_zero = hide_zero .. "0"
     end
-    return table.concat(chars)
+    frameCode.text_black:SetText(black_string)
+    frameCode.text_red:SetText(red_string)
+    frameCode.text_green:SetText(green_string)
+    frameCode.text_blue:SetText(blue_string)
+    frameCode.text_cyan:SetText(cyan_string)
+    frameCode.text_yellow:SetText(yellow_string)
+    frameCode.text_magenta:SetText(magenta_string)
+    frameCode.text_white:SetText(white_string)
+    frameCode.text_hide_zero:SetText(hide_zero)
+    print("Displaying all indices:".. secondsCounter)
+    -- print("B " .. black_string:sub(1, 40))
+    -- print("R " .. red_string:sub(1, 40))
+    -- print("G " .. green_string:sub(1, 40))
+    -- print("B " .. blue_string:sub(1, 40))
+    
 end
 
 
+local base64 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+local utf8_charable = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+local brgb_char = "012345678"
 
-local function setRandomColorBarcodes()
-    frameCode.text_black:SetText(randomText(128))
-    frameCode.text_red:SetText(randomText(128))
-    frameCode.text_green:SetText(randomText(128))
-    frameCode.text_blue:SetText(randomText(128))
-    frameCode.text_magenta:SetText(randomText(128))
-end
-local function setTextCode(text)
-    frameCode.text_black:SetText(text)
-    frameCode.text_red:SetText(text)
-    frameCode.text_green:SetText(text)
-    frameCode.text_blue:SetText(text)
-    frameCode.text_magenta:SetText(text)
-end
-local function setTextCodeTo128()
-    local string_all_chat_1_128 = ""
-    for i = 1, 127 do
-        if i>8 and i<14 then
-            string_all_chat_1_128 = string_all_chat_1_128 .. " "
-        elseif i>127 and i<170 then
-            string_all_chat_1_128 = string_all_chat_1_128 .. " "
-        else 
-            string_all_chat_1_128 = string_all_chat_1_128 .. string.char(i)
-        end
-    end
-    print("string_all_chat_1_128: " .. string_all_chat_1_128)
-
-    frameCode.text_black:SetText(string_all_chat_1_128)
-    frameCode.text_red:SetText(string_all_chat_1_128)
-    frameCode.text_green:SetText(string_all_chat_1_128)
-    frameCode.text_blue:SetText(string_all_chat_1_128)
-    frameCode.text_magenta:SetText(string_all_chat_1_128)
+local get_random_brgb_char = function()
+    local int_index = math.random(1, 1)
+    return brgb_char:sub(int_index, int_index)
 end
 
---local b60 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-"
-local b60 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_-"
-local function setTextCodeTob60()
-    local string_all_chat_1_60 = ""
-    for i = 1, #b60 do
-        string_all_chat_1_60 = string_all_chat_1_60 .. b60:sub(i, i)
-    end
-    print("string_all_chat_1_60: " .. string_all_chat_1_60)
-    frameCode.text_black:SetText(string_all_chat_1_60)
-    frameCode.text_red:SetText(string_all_chat_1_60)
-    frameCode.text_green:SetText(string_all_chat_1_60)
-    frameCode.text_blue:SetText(string_all_chat_1_60)
-    frameCode.text_magenta:SetText(string_all_chat_1_60)
-end
-
-local getRandCharOfB60 = function()
-    local int_index = math.random(1, 58)
-    return b60:sub(int_index, int_index)
-end
-
-local getRandomTextOfB60 = function(length)
+local get_random_text_from_brgb_char = function(length)
     local result = ""
     for i = 1, length do
-        result = result .. getRandCharOfB60()
+        result = result .. get_random_brgb_char()
     end
     return result 
 end
 
-local function setTextRandomB60()
-    test_char_count=40
-    frameCode.text_black:SetText(getRandomTextOfB60(test_char_count))
-    frameCode.text_red:SetText(getRandomTextOfB60(test_char_count))
-    frameCode.text_green:SetText(getRandomTextOfB60(test_char_count))
-    frameCode.text_blue:SetText(getRandomTextOfB60(test_char_count))
-    frameCode.text_magenta:SetText(getRandomTextOfB60(test_char_count))
+local function set_as_random_brgb_char()
+    test_char_count = 60
+    frameCode.text_red:SetText(get_random_text_from_brgb_char(test_char_count))
+    frameCode.text_green:SetText(get_random_text_from_brgb_char(test_char_count))
+    frameCode.text_blue:SetText(get_random_text_from_brgb_char(test_char_count))
+    frameCode.text_black:SetText(b60Black)
 end
 
 
-local secondsCounter = 0
-local function setTextCodeToFrame()
-    local frame_as_char = secondsCounter% 127
-   char_to_display = string.char(frame_as_char+1)
 
-    frameCode.text_black:SetText(char_to_display)
-    frameCode.text_red:SetText(char_to_display)
-    frameCode.text_green:SetText(char_to_display)
-    frameCode.text_blue:SetText(char_to_display)
-    frameCode.text_magenta:SetText(char_to_display)
-    print ("setTextCodeToFrame: " .. char_to_display)
-end
-
-setRandomColorBarcodes()
-
--- -- Normal text
--- local textHeight = 28
--- local textString = frameCode:CreateFontString(nil, "OVERLAY")
--- textString:SetFont("Fonts\\ARIALN.TTF", textHeight)
--- textString:SetPoint("TOP", frameCode, "TOP", 0, -5)
--- textString:SetText("")
--- textString:SetTextColor(0, 0, 0, 1)
--- textString:SetWidth(frameCode:GetWidth() - 20)
--- textString:Show()
-
--- Update font string width on resize
--- frameCode:SetScript("OnSizeChanged", function(self)
---     textString:SetWidth(self:GetWidth() - 20)
--- end)
+ secondsCounter = 0
+local brgb_index_ = 0
 
 local function updateBarcodeAndText()
+    brgb_index_ = brgb_index_ + 1
+    if brgb_index_ >= #brgb_color then
+        brgb_index_ = 0
+    end
+
     local displayText = get_text_to_display()
-    -- setTextCode(encode_to_bare_code_tfb(displayText))
-    --setRandomColorBarcodes()
-    setTextCodeTob60()
-   -- setTextRandomB60()
-    --setTextCodeToFrame()
-    --textString:SetText(experiment_colorize_text(displayText))
+    set_normal_text(displayText)
+    set_text_to_display_brgb(displayText)
+    --set_text_to_display_all_index()
     secondsCounter = secondsCounter + 1
 end
 
-add_to_clock_1_second(updateBarcodeAndText)
+--add_to_clock_1_second(updateBarcodeAndText)
+add_to_clock_10_frames(updateBarcodeAndText)
--- STATIC_LUA_CODE_TO_DISPLAY.lua
+++ STATIC_LUA_CODE_TO_DISPLAY.lua
@@ -2,10 +2,342 @@ G_BARCODE_BACKGROUND_COLOR = {1, 1, 1, 1}
 G_BARCODE_TEXT_COLOR = {0, 0, 0, 1}
 
 function get_text_to_display()
-    local result = [[
-a b c d e  f g h  i j k l  m n o p q r s t u v w x  y z 
-1 2 3 4 5 6 7 8 9 0 $ . % * + - / 
-    ]]
-    result = result .. (feature_get_target_unique_id(false) or "No target")
+
+    local result = ""
+
+-- BASE 64
+--     local result = [[
+-- a b c d e  f g h  i j k l  m n o p q r s t u v w x  y z 
+-- 1 2 3 4 5 6 7 8 9 0 $ . % * + - / 
+--     ]]
+    
+
+-- ALL POSSIBLES UTF8 on the first bits
+-- local result = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+
+
+    result = result .. get_player_map_x_one_char()
+    result = result .. get_player_map_y_one_char()
+    result = result .. get_player_angle360_one_char()
+    result = result .. get_player_life_one_char()
+    result = result .. get_player_mana_or_energy_one_char()
+    result = result .. get_player_xp_percent_one_char()
+    result = result .. get_player_world_map_x_several_char()
+    result = result .. get_player_world_map_y_several_char()
+    result = result .. get_allies12_life_on_4bits()
+    result = result .. get_allies24_life_on_4bits()
+
     return result
+end
+
+
+
+function get_target_life_percent(unit)
+    -- Does not work , I dont know why wiht party1 and party2
+    if not UnitExists(unit) or UnitIsDeadOrGhost(unit) == 1 then
+        return 0
+    end
+    local maxHP = UnitHealthMax(unit)
+    if maxHP == 0 then
+        return 0
+    end
+    return UnitHealth(unit) / maxHP
+end
+
+function get_allies12_life_on_4bits()
+    local life1 = get_target_life_percent("party1")
+    local life2 = get_target_life_percent("party2")
+
+    local ff = getPercentToF(life2).. getPercentToF(life1)
+    return int_to_char(FF_To_Decimal(ff))
+
+end
+
+
+function get_allies24_life_on_4bits()
+    local life1 = get_target_life_percent("party3")
+    local life2 = get_target_life_percent("party4")
+
+    local ff = getPercentToF(life2) .. getPercentToF(life1)
+    return int_to_char(FF_To_Decimal(ff))
+
+end
+
+
+function int_to_char(value)
+    if value == nil then
+        return "0"
+    end
+    local intValue = math.floor(value)
+    if intValue < 0 then
+        intValue = 0
+    elseif intValue > 255 then
+        intValue = 255
+    end
+    return string.char(intValue)
+end
+
+function get_player_life_one_char()
+    local percentHealth = UnitHealth("player") / UnitHealthMax("player")
+    if percentHealth == nil then
+        return 0
+    end
+    return int_to_char(percentHealth * 255)
+end
+
+function get_player_mana_or_energy_one_char()
+    local percentMana = UnitPower("player") / UnitPowerMax("player")
+    if percentMana == nil then
+        return 0
+    end
+    return int_to_char(percentMana * 255)
+end
+
+function get_player_xp_percent_one_char()
+    local xp = UnitXP("player")
+    local percentXp = xp / UnitXPMax("player")
+    if percentXp == nil then
+        return 0
+    end
+    return int_to_char(percentXp * 255)
+end
+
+
+
+
+function get_player_map_x_one_char()
+    local map = C_Map.GetBestMapForUnit("player");
+    local pos = C_Map.GetPlayerMapPosition(map,"player");
+    local posX,_ = pos:GetXY()
+    if posX == nil then
+        return 0
+    end
+    return int_to_char(posX * 255)
+end
+
+function get_player_map_y_one_char()
+    local map = C_Map.GetBestMapForUnit("player");
+    local pos = C_Map.GetPlayerMapPosition(map,"player");
+    local _ ,posY = pos:GetXY()
+    if posY == nil then
+        return 0
+    end
+    return int_to_char(posY * 255)
+end
+
+function get_player_angle360_one_char()
+    local facing = (GetPlayerFacing() / (2*3.1418))
+    return int_to_char(facing * 255)
+end
+
+
+
+-------------------------------------
+
+function parse_float_value_to_signed_short_double_bytes(given_float)
+    -- Take sign into account: store sign in the highest bit of b3
+    local intValue = math.floor(math.abs(given_float) * 10000)
+    local sign = given_float < 0 and 1 or 0
+
+    local b1 = intValue % 256
+    local b2 = math.floor(intValue / 256) % 256
+    local b3 = math.floor(intValue / (256 * 256)) % 128 -- only lower 7 bits
+    b3 = b3 + (sign * 128) -- set highest bit if negative
+
+    return b1, b2, b3
+end
+
+function get_player_world_map_x_several_char()
+    
+    local result =0
+    -- local isInDonjon = IsInInstance()
+    -- if isInDonjon then
+    --     result = 0
+    -- else
+        local map = C_Map.GetBestMapForUnit("player");
+        local pos = C_Map.GetPlayerMapPosition(map,"player");
+        local posX,posY = pos:GetXY()
+        result = posX 
+    -- end
+    
+
+    b1 , b2, b3 = parse_float_value_to_signed_short_double_bytes(result)
+    return int_to_char(b1) .. int_to_char(b2) .. int_to_char(b3)
+end
+
+
+
+function get_player_world_map_y_several_char()
+    
+    local result =0
+    -- local isInDonjon = IsInInstance()
+    -- if isInDonjon then
+    --     result = 0
+    -- else
+        local map = C_Map.GetBestMapForUnit("player");
+        local pos = C_Map.GetPlayerMapPosition(map,"player");
+        local posX,posY = pos:GetXY()
+        result = posY 
+    --end
+    
+
+    b1 , b2, b3 = parse_float_value_to_signed_short_double_bytes(result)
+    return int_to_char(b1) .. int_to_char(b2) .. int_to_char(b3)
+end
+
+
+function getPercentToF(valuePercent)
+   
+    if valuePercent<1.0/15.0 then return '0' end
+    if valuePercent<2.0/15.0 then return '1' end
+    if valuePercent<3.0/15.0 then return '2' end
+    if valuePercent<4.0/15.0 then return '3' end
+    if valuePercent<5.0/15.0 then return '4' end
+    if valuePercent<6.0/15.0 then return '5' end
+    if valuePercent<7.0/15.0 then return '6' end
+    if valuePercent<8.0/15.0 then return '7' end
+    if valuePercent<9.0/15.0 then return '8' end
+    if valuePercent<10.0/15.0 then return '9' end
+    if valuePercent<11.0/15.0 then return 'A' end
+    if valuePercent<12.0/15.0 then return 'B' end
+    if valuePercent<13.0/15.0 then return 'C' end
+    if valuePercent<14.0/15.0 then return 'D' end
+    if valuePercent<15.0/15.0 then return 'E' end
+    return 'F'
+end
+
+function FF_To_Decimal(value)
+    
+ -- turn ff hexa to 255 decimal
+    local decimalValue = tonumber(value, 16)
+    if decimalValue == nil then
+        return 0
+    end
+    return decimalValue
+
+end
+
+function FF_To_Percent(value)
+    local decimalValue = FF_To_Decimal(value)
+    return decimalValue / 255.0
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getPlayerAsColor(selection)
+    local targetGUID = UnitGUID(selection)
+    
+    if not targetGUID then
+        return 1, 1, 1, 1, 1, 1 -- white
+    else
+        local string_ffffffffffff = "000000000000"
+        if not UnitIsPlayer(selection) then
+            if targetGUID and targetGUID:find("Creature") then
+                string_ffffffffffff = "FF0000000000"
+                local unitType, zero, serverId, instanceId, zoneUid, npcId, spawnUid = strsplit("-", targetGUID)
+
+                -- convert npcid in decimal to hexadecimal
+                local idAsDecimal = tonumber(npcId, 10) or 0
+                local hex = string.format("%x", idAsDecimal)
+
+                -- copy from right to left characters to fill the string_ffffffffffff
+                local hexLength = string.len(hex)
+                local startIndex = 12 - hexLength
+                string_ffffffffffff = string.sub(string_ffffffffffff, 1, startIndex) .. hex .. string.sub(string_ffffffffffff, startIndex + hexLength + 1)
+                --print("GUID: " .. targetGUID .. " Hex: " .. string_ffffffffffff)
+            end
+            if targetGUID and targetGUID:find("Vehicle") then
+                string_ffffffffffff = "FD0000000000"
+            end
+            if targetGUID and targetGUID:find("Pet") then
+                string_ffffffffffff = "FE0000000000"
+            end
+        else
+            string_ffffffffffff = string.lower(string.gsub(string.gsub(targetGUID, "-", ""), "Player", ""))
+        end
+        local hex = string.sub(string_ffffffffffff, 1, 12)
+        local c1r = FF_To_Percent(string.sub(hex, 1, 2))
+        local c1g = FF_To_Percent(string.sub(hex, 3, 4))
+        local c1b = FF_To_Percent(string.sub(hex, 5, 6))
+        local c2r = FF_To_Percent(string.sub(hex, 7, 8))
+        local c2g = FF_To_Percent(string.sub(hex, 9, 10))
+        local c2b = FF_To_Percent(string.sub(hex, 11, 12))
+
+        return c1r, c1g, c1b, c2r, c2g, c2b
+    end
+
+    return 1, 1, 1, 1, 1, 1 -- white
+end
+
+
+
+
+function getPlayerAsColorFocus()
+    return getPlayerAsColor("target")
+end
+
+function getPlayerAsColorCurrent()
+    return getPlayerAsColor("player")
+end
+
+
+function getHealAndXp()
+    
+    local percentHealth = UnitHealth("player") / UnitHealthMax("player")
+
+    local playerLevel = UnitLevel("player")/255.0 or 0 
+
+    local xp = UnitXP("player")
+    local percentXp = xp / UnitXPMax("player")
+    
+    return percentHealth, playerLevel   , percentXp
+end
+
+function getWorldPosition(trueXFalseY)
+    local isInDonjon = IsInInstance()
+    local px, py, pz = 0, 0, 0
+
+    -- Fetch player position if not in an instance
+    if not isInDonjon then
+        py, px, pz = UnitPosition("player")
+    end
+
+    -- Choose the coordinate to encode (x or y)
+    local coordinate = trueXFalseY and px or py
+    local r, g, b = 0, 0, 0
+    -- -347321 
+    -- r =34
+    -- g = 73
+    -- b = 21
+    -- if is negative r = r+100
+    local isNegative = coordinate < 0
+    --print ("Coordinate "..coordinate)
+    coordinate = math.abs(coordinate)
+    local b = math.floor(coordinate) %100 
+    local g = math.floor(coordinate / 100.0)%100
+    local r = math.floor(coordinate / 10000.0)%100
+    if isNegative then
+        r = r + 100
+    end
+    -- print ("RGB "..r.." "..g.." "..b)
+    return r/255.0, g/255.0, b/255.0
+
 end
